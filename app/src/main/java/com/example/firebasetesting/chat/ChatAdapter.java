7af173b561c0e87511520e2623ce28b5f8944e7f
Dk146 <dinhkhanh1406@gmail.com>
2021-09-05 10:10:20 +0700

    update chat

--- app/src/main/java/com/example/firebasetesting/chat/ChatAdapter.java
+++ app/src/main/java/com/example/firebasetesting/chat/ChatAdapter.java
@@ -59,10 +59,10 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder
         Log.d("ChatMessage", mChat_list.get(position).getMessage());
         if (mChat_list.get(position).getCurrentUser()) {
             holder.mMessage.setGravity(Gravity.END);
-            holder.mContainer.setBackgroundColor(Color.parseColor("#8db0e0"));
+            holder.mMessage.setBackgroundColor(Color.parseColor("#8db0e0"));
         } else {
             holder.mMessage.setGravity(Gravity.START);
-            holder.mContainer.setBackgroundColor(Color.parseColor("#bec4cc"));
+            holder.mMessage.setBackgroundColor(Color.parseColor("#bec4cc"));
         }
     }
 
